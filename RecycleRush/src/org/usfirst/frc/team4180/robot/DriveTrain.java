61e3d34ca657c03ac20377fe926471091d49f142
ravithegoat <acorns9@gmail.com>
2015-01-14 13:54:22 -0800

    wrote: -constructor -variables -setSpeed
    for robot :3

--- RecycleRush/src/org/usfirst/frc/team4180/robot/DriveTrain.java
+++ RecycleRush/src/org/usfirst/frc/team4180/robot/DriveTrain.java
@@ -1,25 +1,45 @@
 package org.usfirst.frc.team4180.robot;
 
 public class DriveTrain {
-    private static final int TEMP_PORT = 10;
-    private int GEAR_SHIFT_PORT = TEMP_PORT_TWO;
-    private Solenoid gearShift;
-
-    public DriveTrain() {
-        gearShift = new Solenoid(GEAR_SHIFT_PORT);
-    }
-    
-    /**
-     * Turns the shift solenoid on
-     */
-    public void gearShiftSolenoidOn(){
-        gearShift.set(true);
-    }
-    
-    /**
-     * Turns the shift solenoid off
-     */
-    public void gearShiftSolenoidOff(){
-        gearShift.set(false);
-    }
+	
+	private TALON_RIGHT
+	private TALON_LEFT
+	private double JOYSTICK_X
+	private double JOYSTICK_Y
+	
+	public DriveTrain(){
+		TALON_RIGHT= new Talon(0);
+		TALON_LEFT= new Talon(1);
+		JOYSTICK_X = 0.0;
+		JOYSTICK_Y = 0.0;
+	}
+		
+	public void setSpeed(rightTalonSpeed, leftTalonSpeed){
+		TALON_RIGHT.set(rightTalonSpeed);
+		TALON_LEFT.set(leftTalonSpeed);
+	}
+	
 }
+
+
+//    private static final int TEMP_PORT = 10;
+//    private int GEAR_SHIFT_PORT = TEMP_PORT_TWO;
+//    private Solenoid gearShift;
+//
+//    public DriveTrain() {
+//        gearShift = new Solenoid(GEAR_SHIFT_PORT);
+//    }
+//    
+//    /**
+//     * Turns the shift solenoid on
+//     */
+//    public void gearShiftSolenoidOn(){
+//        gearShift.set(true);
+//    }
+//    
+//    /**
+//     * Turns the shift solenoid off
+//     */
+//    public void gearShiftSolenoidOff(){
+//        gearShift.set(false);
+//    }
