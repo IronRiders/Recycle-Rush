164d56080d332f2ab98993456b622195c674feb9
FezGirl <FezGirl@users.noreply.github.com>
2015-01-14 15:12:40 -0800

    ElevatorStart
    Started elevator code.

--- RecycleRush/src/org/usfirst/frc/team4180/robot/Elevator.java
+++ RecycleRush/src/org/usfirst/frc/team4180/robot/Elevator.java
@@ -1,5 +1,15 @@
 package org.usfirst.frc.team4180.robot;
 
 public class Elevator {
+	private WINCH_JAGUAR;
+	private double JOYSTICK_Y;
 
+	public Elevator(){
+		WINCH_JAGUAR= new Jaguar(2);
+		//JOYSRICK_Y = something;
+	}
+	
+	public void setSpeed(speed){
+		WINCH_JAGUAR.set(speed);
+	}
 }
