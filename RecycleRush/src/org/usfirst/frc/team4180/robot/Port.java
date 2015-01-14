69958cd9f001e5d94d5ad3e3c875a98e2fb993a2
overlordnate <nespencer@gmail.com>
2015-01-14 14:19:00 -0800

    Worked on the Issue #8

---
+++ RecycleRush/src/org/usfirst/frc/team4180/robot/Port.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team4180.robot;
+
+public enum Port {
+	LEVEL_ZERO_LIMIT_SWITCH (0), 
+	LEVEL_ONE_LIMIT_SWITCH (1), 
+	LEVEL_TWO_LIMIT_SWITCH (2),
+	LEVEL_THREE_LIMIT_SWITCH (3),
+	LEVEL_FOUR_LIMIT_SWITCH (4),
+	LEVEL_FIVE_LIMIT_SWITCH (5),
+	
+	RIGHT_TALONS (0),
+	LEFT_TALONS (1),
+	ELEVATION_WINCH_JAGUAR (2),
+	
+	PNEUMATIC_GEARBOX_SHIFTING (0),
+	GRIPPER_PNEUMATIC_ACTUATOR (1);	
+	
+	private int PORT_NUMBER;
+	Port(int portNumber){
+		PORT_NUMBER = portNumber;
+	}
+	public int GetPort() {
+		return PORT_NUMBER;
+	}
+}
+
--- RecycleRush/src/org/usfirst/frc/team4180/robot/PortHolder.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team4180.robot;
-
-public class PortHolder {
-	public static final int TEMP_PORT = 10;
-}
