d788e65ee79f57964316289430d747a5b189c998
Thomas Noble <plutonor@gmail.com>
2015-01-14 12:48:45 -0800

    Update DriveTrain.java
    put a solenoid in it to shift gears

--- RecycleRush/src/org/usfirst/frc/team4180/robot/DriveTrain.java
+++ RecycleRush/src/org/usfirst/frc/team4180/robot/DriveTrain.java
@@ -1,5 +1,25 @@
 package org.usfirst.frc.team4180.robot;
 
 public class DriveTrain {
+    private static final int TEMP_PORT = 10;
+    private int GEAR_SHIFT_PORT = TEMP_PORT_TWO;
+    private Solenoid gearShift;
 
+    public DriveTrain() {
+        gearShift = new Solenoid(GEAR_SHIFT_PORT);
+    }
+    
+    /**
+     * Turns the shift solenoid on
+     */
+    public void gearShiftSolenoidOn(){
+        gearShift.set(true);
+    }
+    
+    /**
+     * Turns the shift solenoid off
+     */
+    public void gearShiftSolenoidOff(){
+        gearShift.set(false);
+    }
 }
