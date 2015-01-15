b81d72dfcb6fcb8a2320a5a76f349a28dcee2d41
unknown <ros_fbcarroll@WROSRM21722.seattleschools.org>
2015-01-14 17:09:05 -0800

    Fixing the folder structure so that importing the project with egit works.

--- RecycleRush/src/org/usfirst/frc/team4180/robot/RobotInformationHandler.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team4180.robot;
-
-public class RobotInformationHandler {
-
-}
---
+++ src/org/usfirst/frc/team4180/robot/DriveTrain.java
@@ -0,0 +1,73 @@
+package org.usfirst.frc.team4180.robot;
+
+public class DriveTrain {
+	
+	private TALON_RIGHT;
+	private TALON_LEFT;
+	private double JOYSTICK_X;
+	private double JOYSTICK_Y;
+//    private static final int TEMP_PORT = 10;
+//    private int GEAR_SHIFT_PORT = TEMP_PORT;
+//    private Relay gearShiftRelay;
+//    private Solenoid gearShift;
+	
+	public DriveTrain(){
+//        shiftRelay = new Relay(GEAR_SHIFT_PORT);
+//        shift = new Solenoid(GEAR_SHIFT_PORT);
+		TALON_RIGHT= new Talon(0);
+		TALON_LEFT= new Talon(1);
+		JOYSTICK_X = 0.0;
+		JOYSTICK_Y = 0.0;
+	}
+		
+	public void setSpeed(double rightTalonSpeed, double leftTalonSpeed){
+		TALON_RIGHT.set(rightTalonSpeed);
+		TALON_LEFT.set(leftTalonSpeed);
+	}
+	
+//    /**
+//     * Turns the gearShift solenoid on
+//     */
+//    public void gearShiftSolenoidOn(){
+//        gearShift.set(true);
+//    }
+//    
+//    /**
+//     * Turns on the gearShift relay
+//     */
+//    public void gearShiftRelayOn()
+//    {
+//        gearShiftRelay.set(Relay.Value.kForward);
+//    }
+//
+//    /**
+//     * Gives the gearShift relay the value to reverse
+//     */
+//    public void gearShiftRelayReverse(){
+//        gearShiftRelay.set(Relay.Value.kReverse);
+//    }
+//
+//    /**
+//     * Turns the gearShift relay off
+//     */
+//    public void gearShiftRelayOff() {
+//        gearShiftRelay.set(Relay.Value.kOn);
+//    }
+//    
+//    /**
+//     * Gives the port number of the gearShift relay
+//     * @return the port of the gearShift relay
+//     */
+//    public int getGearShiftRelayPort()
+//    {
+//        return GEAR_SHIFT_PORT;
+//    }
+//
+//  /**
+//     * Turns the gearShift solenoid off
+//     */
+//    public void gearShiftSolenoidOff(){
+//        gearShift.set(false);
+//    }
+}
+
