0daadeeb361dd48431dafacb5d66cb04be996263
ros_Dmlamarca <ros_Dmlamarca@WROSRM21724.seattleschools.org>
2015-01-15 15:44:48 -0800

    I created the JoystickListener Interface

---
+++ src/Listeners/JoystickListener.java
@@ -0,0 +1,16 @@
+package Listeners;
+
+/**
+ * Interface describing actions that a JoystickListener must react to.
+ */
+public interface JoystickListener {
+    
+    /**
+     * This method will be called whenever the joystick is moved
+     * 
+     * @param x the X (+up / -down) position of the joystick
+     * @param y the Y (+left / -right) position of the joystick
+     * @param z the Z position of the joystick
+     */
+    public void joystickMoved(double x, double y, double z);
+}
