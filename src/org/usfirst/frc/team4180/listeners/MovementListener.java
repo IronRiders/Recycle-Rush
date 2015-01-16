c8c892d6f32f31819941ed3c4fc6e5359078c3bd
typeHaller <halle@remash.com>
2015-01-16 15:55:09 -0800

    Close #35. Created a MovementListener class. MUST CREATE UPDATEX UPDATEY AND RECALCVELOCITY METHODS.

---
+++ src/org/usfirst/frc/team4180/listeners/MovementListener.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team4180.listeners;
+
+import org.usfirst.frc.team4180.robot.DriveTrain;
+
+public class MovementListener implements JoystickListener {
+	private DriveTrain driveTrain;
+	
+	@Override
+	public void joystickMoved(double x, double y, double z) {
+		// TODO Auto-generated method stub
+		System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        driveTrain.updateX(x);
+        driveTrain.updateY(y);
+        driveTrain.reCalcVelocity();
+	}
+
+}
