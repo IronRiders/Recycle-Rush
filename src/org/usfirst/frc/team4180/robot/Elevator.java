62a016a3004b39a79344a8170618c5286912aa12
plutonor <plutonor@gmail.com>
2015-01-21 15:11:07 -0800

    Implemented Solenoid code into Elevator

--- src/org/usfirst/frc/team4180/robot/Elevator.java
+++ src/org/usfirst/frc/team4180/robot/Elevator.java
@@ -1,22 +1,20 @@
 package org.usfirst.frc.team4180.robot;
 
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Elevator {
 	private static boolean MOVING;
 	private Jaguar WINCH_JAGUAR;
 	private double JOYSTICK_Y;
-//    private static final int TEMP_PORT = 11;
-//    private int RELAY_PORT_GRIP = TEMP_PORT;
-//    private int GRIP_PORT = TEMP_PORT;
-//    private Relay gripRelay;
-//    private Solenoid grip;
+    private static final int TEMP_PORT = 11;
+    private int GRIP_PORT = TEMP_PORT;
+    private Solenoid grip;
 	
 	public Elevator(){
 		WINCH_JAGUAR= new Jaguar(2);
 		//JOYSRICK_Y = something;
-//        gripRelay = new Relay(RELAY_PORT_GRIP);
-//        grip = new Solenoid(GRIP_PORT);
+        grip = new Solenoid(GRIP_PORT);
 	}
 	
 	public void setSpeed(double speed){
@@ -48,49 +46,18 @@ public class Elevator {
 	public boolean getMoving(){
 		return MOVING;
 	}
-
-//     /**
-//     * Gives the grip relay the value to reverse
-//     */
-//   public void gripRelayReverse(){
-//        gripRelay.set(Relay.Value.kReverse);
-//    }
-//
-//    /**
-//     * Turns on the grip relay
-//     */
-//    public void gripRelayOn()
-//    {
-//        gripRelay.set(Relay.Value.kForward);
-//    }
-//    
-//    /**
-//     * Turns the grip relay off
-//     */
-//    public void gripRelayOff() {
-//        gripRelay.set(Relay.Value.kOn);
-//    }
-//    
-//    /**
-//     * Gives the port number of the grip relay
-//     * @return the port of the grip relay
-//     */
-//    public int getGripRelayPort()
-//    {
-//        return RELAY_PORT_GRIP;
-//    }
-//    
-//    /**
-//     * Turns the grip solenoid on
-//     */
-//    public void gripSolenoidOn(){
-//        grip.set(true);
-//    }
-//    
-//    /**
-//     * Turns the grip solenoid off
-//     */
-//    public void gripSolenoidOff(){
-//        grip.set(false);
-//    }
+    
+    /**
+     * Turns the grip solenoid on
+     */
+    public void gripSolenoidOn(){
+        grip.set(true);
+    }
+    
+    /**
+     * Turns the grip solenoid off
+     */
+    public void gripSolenoidOff(){
+        grip.set(false);
+    }
 }
