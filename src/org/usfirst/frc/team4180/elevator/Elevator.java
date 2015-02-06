2bffa0d9ea1e53933e49002f016d5582be725307
plutonor <plutonor@gmail.com>
2015-02-06 15:12:13 -0800

    Commented elevator

--- src/org/usfirst/frc/team4180/elevator/Elevator.java
+++ src/org/usfirst/frc/team4180/elevator/Elevator.java
@@ -20,32 +20,52 @@ public class Elevator {
    private DoubleSolenoid grip;
     private final double winchSpeed = 0.2;
 	
-	public Elevator(){
+    /**
+     * The constructor for the Elevator class which controls the elevator on the robot
+     * It assigns the ports for the elevation solenoid and the Jaguar.
+     */
+    public Elevator(){
 		WINCH_JAGUAR = new Jaguar(Port.ELEVATION_WINCH_JAGUAR.GetPort());
         grip = new DoubleSolenoid(Port.GRIPPER_PNEUMATIC_ACTUATOR_FORWARD.GetPort(), Port.GRIPPER_PNEUMATIC_ACTUATOR_REVERSE.GetPort());
 	}
 	
-	public void setSpeed(double speed){
+    /**
+     * Gives the Jaguar the value for speed
+     */
+ 	public void setSpeed(double speed){
 		WINCH_JAGUAR.set(speed);
 	}
 	
-	public void raiseArm(){
+    /**
+     * calls setSpeed and sends it to the correct speed
+     */
+ 	public void raiseArm(){
 		setSpeed(winchSpeed);
 	}
 	
-	public void lowerArm(){
+    /**
+     * calls setSpeed and sends it to the correct speed
+     */
+ 	public void lowerArm(){
 		setSpeed(-winchSpeed);
 	}
 	
 
-	public void stopWinch() {
+    /**
+     * calls setSpeed and sends it to 0
+     */
+ 	public void stopWinch() {
 		setSpeed(0);
 	}
 	
-	public boolean getMoving(){
+    /**
+     * Returns whether the elevator is currently moving in the form of a boolean
+     */
+  	public boolean getMoving(){
 		return MOVING;
 	}
 	
+//  	REQUIRES COMMENTS----------------------------------------------------------------------
 	public void updateLimitSwitches() {
 		STATUS_OF_BOTTOM_SWITCH = LIMIT_SWITCH_BOTTOM.get();
 		STATUS_OF_TOP_SWITCH = LIMIT_SWITCH_TOP.get();
@@ -75,6 +95,9 @@ public class Elevator {
         grip.set(DoubleSolenoid.Value.kOff);
     }
     
+    /**
+     *  Gives the grip relay the value to reverse
+     */
     public void gripSolenoidReverse(){
         grip.set(DoubleSolenoid.Value.kReverse);
     }
