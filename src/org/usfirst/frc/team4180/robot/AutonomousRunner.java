4e68cbbf97e79ef4b6e096d6a3d953a9494743bf
FezGirl <fi.grasshopper@gmail.com>
2015-02-06 16:30:30 -0800

    We added autonmousSpeed and timer and wrote in the driveToAutoZone method -Dodo, Banana, Small Pox (Awesome Team Awesome)

--- src/org/usfirst/frc/team4180/robot/AutonomousRunner.java
+++ src/org/usfirst/frc/team4180/robot/AutonomousRunner.java
@@ -3,14 +3,18 @@ package org.usfirst.frc.team4180.robot;
 import org.usfirst.frc.team4180.elevator.Elevator;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 
 public class AutonomousRunner {
     private DigitalInput AUTO_SWITCH_1 = new DigitalInput(Port.AUTO_SWITCH_1.GetPort());
     private DigitalInput AUTO_SWITCH_2 = new DigitalInput(Port.AUTO_SWITCH_2.GetPort());
 	private DriveTrain DRIVE_TRAIN; 
     private Elevator ELEVATOR;
+    private double autonomousSpeed; 
+    private Timer timer = new Timer();
     
     public AutonomousRunner() {
+    	
     	boolean switchOneValue = AUTO_SWITCH_1.get();
     	boolean switchTwoValue = AUTO_SWITCH_2.get();
     	if(switchOneValue && switchTwoValue) {
@@ -22,17 +26,23 @@ public class AutonomousRunner {
     	} else {
     		disableAutonomous();
     	}
+    	autonomousSpeed = 0.5; // # to be determined
     }
     
     private void driveToAutoZone() {
 //    	start in starting zone
 //    	drive into auto zone
+    	Robot.driveTrain.setSpeed(autonomousSpeed);
+    	timer.delay(5); // # to be determined
+    	Robot.driveTrain.stopRobot();
+    	
     }
     
     private void moveToteToAutoZone() {
 //    	start facing tote
 //    	pick up bin, rotate
 //    	drive to autozone
+    	
     	
     }
     
