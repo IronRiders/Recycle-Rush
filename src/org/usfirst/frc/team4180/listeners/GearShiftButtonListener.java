9f85eb012cd7cab92838ce04a9dd2d855f39f487
Dylan LaMarca <dylan@lamarca.org>
2015-02-14 13:45:49 -0800

    Made GearShift toggle on buttonDown

--- src/org/usfirst/frc/team4180/listeners/GearShiftButtonListener.java
+++ src/org/usfirst/frc/team4180/listeners/GearShiftButtonListener.java
@@ -4,16 +4,22 @@ import org.usfirst.frc.team4180.robot.DriveTrain;
 
 public class GearShiftButtonListener implements ButtonListener{
 	private DriveTrain driving;
+	private boolean open;
 	
 	public GearShiftButtonListener(DriveTrain newDriveTrain){
 		driving = newDriveTrain;
+		open = true;
 	}
 	@Override
 	public void buttonDown(){
- 		driving.gearShiftSolenoidOn();
+		if(open) {
+			driving.gearShiftSolenoidReverse();
+		} else {
+			driving.gearShiftSolenoidOn();
+		}
+		open = !open;
 	}
+	
 	@Override
-	public void buttonUp(){
-		driving.gearShiftSolenoidOff();
-	}
+	public void buttonUp() {}
 }
