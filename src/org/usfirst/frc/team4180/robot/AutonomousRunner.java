ad6655991196b173bc399541aa947434e3b7516b
Iron Rider <studentleaders@ironriders.org>
2015-03-11 17:22:11 -0700

    Created turnDegrees and moveDistance methods. Also created additional autonomous method (untested) for bin and tote manipulation.

--- src/org/usfirst/frc/team4180/robot/AutonomousRunner.java
+++ src/org/usfirst/frc/team4180/robot/AutonomousRunner.java
@@ -6,81 +6,98 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Timer;
 
 public class AutonomousRunner {
-    private static final double AUTONOMOUS_SPEED = 0.3;
-    private AutonomousDial dial;
+    private static final double AUTONOMOUS_SPEED = 0.5;
+//    private AutonomousDial dial;
     private DriveTrain driveTrain;
     private Elevator elevator;
+	private static final double SPEED_ON_TILE = 25.0;
+	private static final double TURNING_RATE_ON_TILE = 115.0/2.0; //degree per second
     
     public AutonomousRunner(DriveTrain driveTrain, Elevator elevator) {
     	this.elevator = elevator;
     	this.driveTrain = driveTrain;
-    	this.dial = new AutonomousDial(11, 12, 13, 14);
-    	
-    }
-    
-    public void setAutonomous() {
-    	int position = dial.getPosition();
-    	switch(position) {
-    	case 1:
-    		disableAutonomous();
-    		break;
-    	case 2:
-    		driveToAutoZone();
-    		break;
-    	case 3:
-    		moveToteToAutoZone();
-    		break;
-    	case 4:
-    		moveBinToAutoZone();
-    		break;
-    	}
-    }
-    
-    public AutonomousDial getDial() {
-    	return dial;
+//    	this.dial = new AutonomousDial(2,3,4,5);
+//      public void setAutonomous() {
+//    	int position = dial.getPosition();
+//    	switch(position) {
+//    	case 1:
+//    		disableAutonomous();
+//    		break;
+//    	case 2:
+//    		driveToAutoZone();
+//    		break;
+//    	case 3:
+//    		moveToteToAutoZone();
+//    		break;
+//    	case 4:
+//    		moveBinToAutoZone();
+//    		break;
+//    	}
+//    }
+//    
+//    public AutonomousDial getDial() {
+//    	return dial;
+//    }
     }
     
     public void driveToAutoZone() {
-//    	start in starting zone
-//    	drive into auto zone
-    	driveTrain.setSpeed(-AUTONOMOUS_SPEED);
-    	Timer.delay(3); //TODO: test for delay
-    	driveTrain.stopRobot();
-    	
+////    start in starting zone facing the autozone
+		moveDistance(115);
     }
     
+    //precondition: facing tote.
     public void moveToteToAutoZone() {
-//    	start facing tote
-//    	pick up tote, rotate
-//    	drive to autozone
+    	
     	elevator.gripSolenoidOn();
-    	Timer.delay(1.5);
-    	elevator.setSpeed(-.75);;
-    	Timer.delay(2); //TODO: test for delay
-    	elevator.stopWinch(); // change this after holding point is established
-    	driveTrain.setSpeed(-0.5, -0.5); //TODO: to be tested
-    	Timer.delay(0.35); //TODO: test for delay
-    	driveTrain.setSpeed(-AUTONOMOUS_SPEED);
-    	Timer.delay(4); //TODO: test for delay
-    	//driveTrain.stopRobot();
+    	Timer.delay(.6); //TODO:test
+    	turnDegree(90);
+    	driveToAutoZone();
     }
     
     public void moveBinToAutoZone() {
-//    	start facing bin
-//    	pick up bin, rotate
-//    	drive to autozone
-    	elevator.gripSolenoidOn();
-    	Timer.delay(1.5);
-    	elevator.setSpeed(-.75);
-    	Timer.delay(1.5); //TODO: test for delay
-    	elevator.stopWinch(); // change this after holding point is established
-    	elevator.setSpeed(Robot.ELEVATOR_HOLD_POSITION_SPEED);
-    	driveTrain.setSpeed(-0.5, 0.5); //TODO: to be tested turning
-    	Timer.delay(1); //TODO: test for delay
-    	driveTrain.setSpeed(-AUTONOMOUS_SPEED);
-    	Timer.delay(3); //TODO: test for delay
-    	driveTrain.stopRobot();
+////    	start facing bin
+    	//	grip bin
+    		elevator.gripSolenoidOn();
+    		Timer.delay(.6); //TODO:test
+    		elevator.stopWinch();
+    		turnDegree(-90);
+    		driveToAutoZone();
     } 
+    
+    public void toteAndBinToAutoZone(){
+    	//grip bin
+    	elevator.gripSolenoidOn();
+    	Timer.delay(.6);//TODO:test
+    	elevator.stopWinch();
+    	//TODO: create a method in elevator which defines a distance to raise the arm
+    	elevator.raiseArm(); 
+    	Timer.delay(.6);//TODO:test
+    	elevator.stopWinch();
+    	moveDistance(20);
+    	//set bin on tote
+    	//TODO: create a method in elevator which defines a distance to lower the arms
+    	elevator.lowerArm(); 
+    	Timer.delay(.6);//TODO:test
+    	elevator.stopWinch();
+    	turnDegree(90);
+    	driveToAutoZone();
+    }
+    
+    private void moveDistance(double inches) { //calibrated for tile
+    	driveTrain.setSpeed(-AUTONOMOUS_SPEED, -AUTONOMOUS_SPEED);
+    	Timer.delay(inches/SPEED_ON_TILE);
+    	driveTrain.stopRobot();
+    }
+    
+    private void turnDegree(double degree) { //calibrated for tile
+    	if(degree >= 0) { //greater than zero turns to the right
+    		driveTrain.setSpeed(AUTONOMOUS_SPEED, -AUTONOMOUS_SPEED); 
+    	} else { //this turns it to the left
+        	driveTrain.setSpeed(-AUTONOMOUS_SPEED, AUTONOMOUS_SPEED); 
+    	}
+    	Timer.delay(Math.abs(degree)/TURNING_RATE_ON_TILE);
+    	driveTrain.stopRobot();
+    }
     
     public void disableAutonomous() {
     	
